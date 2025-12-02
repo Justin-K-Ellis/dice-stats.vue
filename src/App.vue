b95267dea8e14b3fd327aeddedb1f638bd04d982
Justin-K-Ellis <jlke45@gmail.com>
2025-12-02 22:36:43 +0900

    feat: let user add data to the rolls count

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import rollDice from './utils/rollDice'
+import type { RollStats } from '../types'
+
+const diceStats = ref<RollStats>({})
+
+function handleRoll(): void {
+  const roll = rollDice()
+  if (!diceStats.value[roll]) {
+    diceStats.value[roll] = '#'
+  } else {
+    diceStats.value[roll] += '#'
+  }
+}
+</script>
 
 <template>
   <main>
     <h1>Dice Stats</h1>
+    <div class="controls">
+      <button @click="handleRoll">Roll</button>
+    </div>
+    <article>
+      <p v-for="(rolls, num) in diceStats" :key="num">{{ num }}: {{ rolls }}</p>
+    </article>
   </main>
 </template>
 
@@ -22,9 +43,19 @@ main {
   border-left: 1px solid white;
   border-right: 1px solid white;
   padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 h1 {
   text-align: center;
+}
+
+.controls {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  justify-content: center;
 }
 </style>
---
+++ types.ts
@@ -0,0 +1,3 @@
+export interface RollStats {
+  [key: number]: string
+}
